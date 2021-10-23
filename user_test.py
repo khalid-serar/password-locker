66f2b35065ccd87e381b9fb0e56ec7e6ad90dfb9
khalid-serar <khalid.serar@student.moringaschool.com>
2021-10-23 09:15:26 +0300

    commit changes

--- user_test.py
+++ user_test.py
@@ -30,3 +30,13 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+
+        def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []  
+
+
+if __name__ == '__main__':
+    unittest.main()
