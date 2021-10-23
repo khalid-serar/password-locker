bb555c148dd48df05e2e0048b1aaa945bff4d1bb
khalid-serar <khalid.serar@student.moringaschool.com>
2021-10-23 09:11:01 +0300

    add changes

--- user.py
+++ user.py
@@ -12,3 +12,9 @@ class User:
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
+
+        def save_user(self):
+        '''
+        save_user method that saves new user object
+        '''
+        User.user_list.append(self)
