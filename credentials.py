acaa458bd6cab1d055fea855429a470a3fe476e7
khalid-serar <khalid.serar@student.moringaschool.com>
2021-10-23 09:18:19 +0300

    add credentials

--- credentials.py
+++ credentials.py
@@ -11,3 +11,10 @@ class Credentials:
     
     credential_list = []
     
+     def __init__(self,user_name,account_name,password):
+        '''
+        Method to define the properties for each user object.
+        '''
+        self.user_name = user_name
+        self.account_name = account_name
+        self.password = password
