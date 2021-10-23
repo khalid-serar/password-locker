6e034c422b366a0f831b9dc24c20ee76b370a2e3
khalid-serar <khalid.serar@student.moringaschool.com>
2021-10-23 09:12:37 +0300

    add latest changes

---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from user import User #importing user class
+
+
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+    '''
+    
