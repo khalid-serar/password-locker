cbf3a15ba3c003266a23141979293b7e70fc8614
khalid-serar <khalid.serar@student.moringaschool.com>
2021-10-23 09:14:19 +0300

    modify user.py file

--- user_test.py
+++ user_test.py
@@ -9,3 +9,24 @@ class TestUser(unittest.TestCase):
     Test class that defines test cases for the user class behaviours.
     '''
     
+     def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        
+        self.new_user = User('Maalim', '2345678')
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"Maalim")
+        self.assertEqual(self.new_user.password,"2345678")
+    
+    def test_save_user(self):
+        '''
+        Test to check if the new users info is saved into the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
