2312f3e46f7c62bfb7bb2cee108e37cfca255fcf
khalid-serar <khalid.serar@student.moringaschool.com>
2021-10-23 09:08:35 +0300

    add modified file

---
+++ user.py
@@ -0,0 +1,14 @@
+import pyperclip
+
+
+
+class User:
+    '''
+    class that allows user to generate user account
+    '''
+    
+    user_list = []
+    
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
